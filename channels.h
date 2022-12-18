58c0ac7e4d45fbadba0f2b3f1ca2a07d187cdda6
dlawle <39495124+dlawle@users.noreply.github.com>
2022-12-18 10:34:21 -0500

    notes for attempted step visualizer

--- channels.h
+++ channels.h
@@ -1,1 +1,37 @@
 // contains struct information for each channel. 
+void incrOled(int s) {
+  switch (s) {
+          case 0:
+              display.drawRect(28,channelYpos[0]-2,10,64,SSD1306_WHITE);
+              break;
+
+          case 1:
+              display.drawRect(40,channelYpos[0]-2,10,64,SSD1306_WHITE);
+              break;
+
+          case 2:
+              display.drawRect(52,channelYpos[0]-2,10,64,SSD1306_WHITE);
+              break;
+
+          case 3:
+              display.drawRect(64,channelYpos[0]-2,10,64,SSD1306_WHITE);
+              break;
+
+          case 4:
+              display.drawRect(76,channelYpos[0]-2,10,64,SSD1306_WHITE);
+              break;
+
+          case 5:
+              display.drawRect(88,channelYpos[0]-2,10,64,SSD1306_WHITE);
+              break;
+
+          case 6:
+              display.drawRect(100,channelYpos[0]-2,10,64,SSD1306_WHITE);
+              break;
+
+          case 7:
+              display.drawRect(112,channelYpos[0]-2,10,64,SSD1306_WHITE);
+              break;
+  }
+  display.display();
+}
